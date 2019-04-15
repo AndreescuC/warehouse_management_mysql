10a92c8ea92c0b6eae972ef3deeb8960d0e3e4d3
AndreescuC <andreescuconstantin.ac95@gmail.com>
2019-04-15 09:35:55 +0300

    Added an extra shipment in fixtures

--- migrations/fixtures_shipments.sql
+++ migrations/fixtures_shipments.sql
@@ -4,4 +4,5 @@ INSERT INTO `shipment` (`warehouse_id`, `status`, `date`, `no_of_orders`, `city`
 (2, 2, '2018-12-30 00:00:00', 1, 'Bucharest'),
 (2, 2, '2018-12-30 00:00:00', 1, 'Bucharest'),
 (2, 2, '2018-12-30 00:00:00', 1, 'Bucharest'),
-(3, 0, '2018-12-30 00:00:00', 1, 'Bucharest');
+(3, 0, '2018-12-30 00:00:00', 1, 'Bucharest'),
+(3, 1, '2018-12-30 00:00:00', 1, 'Bucharest');
